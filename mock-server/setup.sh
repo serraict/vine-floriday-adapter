9134f583b7501611f88deebd9f7c5fbe0e303a20
marijn <marijn@serraict.com>
2024-08-23 15:59:08 +0200

    Add some client calls
    This does not work too well, however.

--- mock-server/setup.sh
+++ mock-server/setup.sh
@@ -1,3 +1,5 @@
 npm install -g @stoplight/prism-cli
 curl -o swagger.json https://api.staging.floriday.io/suppliers-api-2024v1/swagger/UUID/swagger.json
 prism mock swagger.json --port 9876
+curl http://localhost:9876/additional-services
+curl -H "Accept: application/json" http://127.0.0.1:9876/weekly-supply-line-counters
