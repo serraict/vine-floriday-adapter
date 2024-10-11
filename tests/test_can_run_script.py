00fc3e26d52fdf49f21d75857b8fe560c1fd279a
marijn <marijn@serraict.com>
2024-10-11 10:12:36 +0200

    Update tests to reflect CLI structure changes

--- tests/test_can_run_script.py
+++ tests/test_can_run_script.py
@@ -1,9 +1,21 @@
 import subprocess
 
 
-def test_can_run_script():
+def test_can_run_about():
     assert subprocess.run(["floridayvine", "about"]).returncode == 0
 
 
 def test_can_print_version():
     assert subprocess.run(["floridayvine", "about", "version"]).returncode == 0
+
+
+def test_can_show_info():
+    assert subprocess.run(["floridayvine", "about", "show-info"]).returncode == 0
+
+
+def test_can_run_sync():
+    assert subprocess.run(["floridayvine", "sync"]).returncode == 0
+
+
+def test_can_run_db():
+    assert subprocess.run(["floridayvine", "db"]).returncode == 0
--- tests/test_floriday.py
+++ tests/test_floriday.py
@@ -5,17 +5,17 @@ pytestmark = pytest.mark.integration
 
 
 def test_can_connect_to_floriday():
-    cp = subprocess.run(["floridayvine", "floriday", "floriday-connection-info"])
+    cp = subprocess.run(["floridayvine", "about", "show-info"])
     assert cp.returncode == 0
 
 
-def test_can_print_direct_sales():
-    cp = subprocess.run(["floridayvine", "floriday", "print-direct-sales"])
+def test_can_list_direct_sales():
+    cp = subprocess.run(["floridayvine", "inventory", "list-direct-sales"])
     assert cp.returncode == 0
 
 
-def test_can_print_trade_items():
-    cp = subprocess.run(["floridayvine", "floriday", "print-trade-items"])
+def test_can_list_trade_items():
+    cp = subprocess.run(["floridayvine", "inventory", "list-trade-items"])
     assert cp.returncode == 0
 
 
